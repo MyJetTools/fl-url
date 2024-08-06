3088a5aad3275b88763197a9df891fc972b480f9
amigin <amigin@gmail.com>
2024-08-06 13:36:40 +0300

    Debugging

--- src/http_client/connect_to_http_over_ssh.rs
+++ src/http_client/connect_to_http_over_ssh.rs
@@ -20,13 +20,13 @@ pub async fn connect_to_http_over_ssh(
 ) -> Result<(Arc<SshSession>, SendRequest<Full<Bytes>>), FlUrlError> {
     let ssh_session = if let Some(ssh_sessions_pool) = ssh_sessions_pool {
         println!(
-            "Trying to reuse new remote http host: {}:{} over SSH",
+            "Trying to reuse new SSH Session: {}:{}",
             remote_host, remote_port
         );
         ssh_sessions_pool.get_or_create(ssh_credentials).await
     } else {
         println!(
-            "Establishing new remote http host: {}:{} over SSH",
+            "Establishing new SSH session: {}:{} ",
             remote_host, remote_port
         );
         Arc::new(SshSession::new(ssh_credentials.clone()))
